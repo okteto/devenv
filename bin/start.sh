4a7aa41ac39a5803d6447ffb1fdda2f5365c747e
Ramiro Berrelleza <rberrelleza@gmail.com>
2019-12-11 17:21:16 +0100

    start remote in background

--- bin/start.sh
+++ bin/start.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-set -e
+set -ex
+remote=0
+while getopts ":r" opt; do
+  case $opt in
+    r)
+      remote=1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
 
 echo "Creating marker file $OKTETO_MARKER_PATH ..."
 touch $OKTETO_MARKER_PATH
@@ -9,5 +21,11 @@ syncthingHome=/var/syncthing
 echo "Copying configuration files to $syncthingHome ..."
 cp /var/syncthing/secret/* $syncthingHome
 
+if [ $remote -eq 1 ]; then
+echo "Executing remote..." 
+/var/okteto/bin/remote &
+fi
+
 echo "Executing syncthing..." 
 /var/okteto/bin/syncthing -home $syncthingHome -gui-address 0.0.0.0:8384 -verbose
+
